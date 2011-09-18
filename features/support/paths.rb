bcb9aa0ae05608defc5f43f346c844efccfd766d
Simon Olsberg <simon@olsbergfamily.net>
2011-09-18 18:10:36 -0400

    Added title functionality for show page

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,6 +16,9 @@ module NavigationHelpers
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
+    
+    when /the project page for "([^\"]*)"/ 
+      project_path(Project.find_by_name!($1))
 
     else
       begin
