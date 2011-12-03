567ff02cee37dedae309e63123b1c2f31e1258e5
Simon Olsberg <simon@olsbergfamily.net>
2011-12-03 11:22:32 -0500

    "Made projects only visible to users with permission to see them

--- features/viewing_tickets.feature
+++ features/viewing_tickets.feature
@@ -8,10 +8,13 @@ Feature: Viewing tickets
 			| email							| password 	|
 			| user@ticketee.com | password	|
 		And there is a project called "TextMate 2" 
+		And "user@ticketee.com" can view the "TextMate 2" project
+    And I am signed in as them
 		And "user@ticketee.com" has created a ticket for this project:
 			| title										| description										|
 			| Make it shiny! 					| Gradients! Starbursts! Oh my! |
 		And there is a project called "Internet Explorer"
+		And "user@ticketee.com" can view the "Internet Explorer" project
 		And "user@ticketee.com" has created a ticket for this project:
 			| title										| description			|
 			| Standards compliance 		| Isn't a joke. 	|
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -6,6 +6,7 @@ describe ProjectsController do
   let(:project) { Factory(:project) }
   
   it "displays an error message when asked for a missing project" do
+    sign_in(:user, user)
     get :show, :id => "not-here"
     response.should redirect_to(projects_path)
     message = "The project you were looking for could not be found."
@@ -25,5 +26,13 @@ describe ProjectsController do
         flash[:alert].should == "You must be an admin to do that."
       end
     end
+    
+    it "cannot access the show action" do
+      sign_in(:user, user)
+      get :show, :id => project.id
+      response.should redirect_to(projects_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
   end
+  
 end
