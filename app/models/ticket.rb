84b7b223828165e78e1a1b7d64e3692a5fe5041c
Simon Olsberg <simon@olsbergfamily.net>
2011-12-15 22:47:06 -0500

    Users can now upload 3 files at a time

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,5 +4,6 @@ class Ticket < ActiveRecord::Base
   validates :title, :presence => true
   validates :description, :presence => true,
                           :length => { :minimum => 10 }
-  has_attached_file :asset
+  has_many :assets
+  accepts_nested_attributes_for :assets
 end
